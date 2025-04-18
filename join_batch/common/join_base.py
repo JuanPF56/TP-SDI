c8ee889569878ffec423950e3199c0acc33907d3
JuanPF56 <jpfresia@fi.uba.ar>
2025-04-18 20:04:28 -0300

    feat: implement multiprocessing for receiving and updating movies table in JoinBatchBase

--- join_batch/common/join_base.py
+++ join_batch/common/join_base.py
@@ -1,14 +1,21 @@
+import json
+import os
 import pika
 import multiprocessing
 import signal
+
+from common import logger
 
 class JoinBatchBase:
     def __init__(self, config):
         self.config = config
         self.connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
         self.channel = self.connection.channel()
-        # TODO: Sync mechanism to store movies table once received
-        self.movies_table = None
+        self.manager = multiprocessing.Manager()
+        self.movies_table = self.manager.list()
+        self.movies_table_condition = self.manager.Condition()
+
+        self.table_receiver = multiprocessing.Process(target=self.receive_movies_table)
 
         # Register signal handler for SIGTERM signal
         signal.signal(signal.SIGTERM, self.__handleSigterm)
@@ -16,17 +23,19 @@ class JoinBatchBase:
     def __handleSigterm(self, signum, frame):
         print("SIGTERM signal received. Closing connection...")
         self.connection.close()
+        self.channel.stop_consuming()
+        self.channel.close()
+        self.manager.shutdown()
+        os.kill(self.table_receiver.pid, signal.SIGTERM)
+        self.table_receiver.join()
 
     def process(self):
         # Start the process to receive the movies table
-        process = multiprocessing.Process(target=self.receive_movies_table)
-        process.start()
+        self.table_receiver.start()
 
         # Start the loop to receive the batches
-        self.receive_batch()        
+        self.receive_batch()
 
-        process.join()
-        
 
     def receive_movies_table(self):
         # Declare a fanout exchange
@@ -41,14 +50,31 @@ class JoinBatchBase:
 
         # Callback function to handle incoming messages
         def callback(ch, method, properties, body):
-            # TODO: Safely store table
-            self.movies_table = body           
+            # Process the incoming message (the movies table)
+            # TODO: Use own protocol to decode the message
+            movies_table = body.decode('utf-8')
+            movies_table = json.loads(movies_table)
 
+            logger.info(f"Received movies table: {movies_table}")
+
+
+            # Update the shared movies table with the new data
+            with self.movies_table_condition:
+                # Add the new movies to the shared movies table
+                self.movies_table.extend(movies_table["movies"])                              
+                logger.info("Movies table updated")
+            
+            if movies_table["last"]:
+                logger.info("Last movies table received.")
+                self.channel.stop_consuming()
+                self.movies_table_condition.notify_all()
+                
+                
         # Start consuming messages from the queue
         self.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
         
         self.channel.start_consuming()
 
     def receive_batch(self):
-        pass
+        raise NotImplementedError("Subclasses should implement this method.")
         
