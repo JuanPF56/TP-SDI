8b3cb683fb92a918b94bed6f89c283b348a48018
csebellin@fi.uba.ar <csebellin@fi.uba.ar>
2025-04-19 22:45:08 -0300

    Refactor: in send_datasets_to_server remove of repeated code

--- client/utils.py
+++ client/utils.py
@@ -17,27 +17,21 @@ def download_dataset():
         return None
 
 def send_datasets_to_server(datasets_path: str, protocol: ProtocolClient):
-        protocol.send_dataset(datasets_path, "movies_metadata", "BATCH_MOVIES")
-        receive_confirmation = protocol.receive_confirmation()
-        if receive_confirmation != ACK:
-            logger.error("Server returned an error after sending movies.")
-            raise Exception("Server returned an error after sending movies.")
-        logger.info("Movies were sent successfully.")
-        
+    datasets = [
+        ("movies_metadata", "BATCH_MOVIES", "movies"),
+        ("credits", "BATCH_CREDITS", "actors"),
+        ("ratings", "BATCH_RATINGS", "ratings"),
+    ]
 
-        protocol.send_dataset(datasets_path, "credits", "BATCH_CREDITS")
-        receive_confirmation = protocol.receive_confirmation()
-        if receive_confirmation != ACK:
-            logger.error("Server returned an error after sending actors.")
-            raise Exception("Server returned an error after sending actors.")
-        logger.info("Actors were sent successfully.")
+    for filename, tag, description in datasets:
+        logger.info(f"Sending {description} dataset...")
+        protocol.send_dataset(datasets_path, filename, tag)
         
-        protocol.send_dataset(datasets_path, "ratings", "BATCH_RATINGS")
-        receive_confirmation = protocol.receive_confirmation()
-        if receive_confirmation != ACK:
-            logger.error("Server returned an error after sending ratings.")
-            raise Exception("Server returned an error after sending ratings.")
-        logger.info("Ratings were sent successfully.")
+        confirmation = protocol.receive_confirmation()
+        if confirmation != ACK:
+            logger.error(f"Server returned an error after sending {description}.")
+            raise Exception(f"Server returned an error after sending {description}.")
         
-        
-        logger.info("All datasets were sent.")
+        logger.info(f"{description.capitalize()} dataset sent successfully.")
+
+    logger.info("All datasets were sent successfully.")
