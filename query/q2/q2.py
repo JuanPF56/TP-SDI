61b78fe37270b95fb1c7bf6c3135b82412ea4ed3
nathencinoza <nencinoza@fi.uba.ar>
2025-04-27 22:22:54 -0300

    q1 uses mom

--- query/q2/q2.py
+++ query/q2/q2.py
@@ -2,7 +2,7 @@ import configparser
 import json
 import os
 import pika
-
+from common.mom import RabbitMQProcessor
 from collections import defaultdict
 from common.logger import get_logger
 
@@ -17,18 +17,16 @@ class SoloCountryBudgetQuery:
     def __init__(self, config):
         self.config = config
         self.budget_by_country = defaultdict(int)
-        rabbitmq_host = self.config["DEFAULT"].get("rabbitmq_host", "rabbitmq")
-        input_queue = self.config["DEFAULT"].get("movies_solo_queue", "movies_solo")
-
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
-        self.channel = self.connection.channel()
+        self.source_queue = self.config["DEFAULT"].get("movies_solo_queue", "movies_solo")
+        self.target_queue = self.config["DEFAULT"].get("results_queue", "results_queue")
 
         self.eos_to_await = int(os.getenv("NODES_TO_AWAIT", "1"))
         self.eos_flags = {}
-
-        self.channel.queue_declare(queue=input_queue)
-        self.channel.queue_declare(queue=config["DEFAULT"]["results_queue"])
-
+        self.rabbitmq_processor = RabbitMQProcessor(
+            config=self.config,
+            source_queues=self.source_queue,
+            target_queues=self.target_queue
+        )   
 
 
     def _calculate_and_publish_results(self):
@@ -42,11 +40,57 @@ class SoloCountryBudgetQuery:
             "results": top_5
         }
         logger.info("RESULTS:" + str(results))
-        self.channel.basic_publish(
-            exchange='',
-            routing_key=self.config["DEFAULT"]["results_queue"],
-            body=json.dumps(results),
+        self.rabbitmq_processor.publish(
+            queue=self.config["DEFAULT"]["results_queue"],
+            message=results
         )
+
+
+    def callback(self, ch, method, properties, body, input_queue):
+        try:
+            msg_type = properties.type if properties and properties.type else "UNKNOWN"
+
+            if msg_type == EOS_TYPE:
+                try:
+                    data = json.loads(body)
+                    node_id = data.get("node_id")
+                except json.JSONDecodeError:
+                    logger.error("Failed to decode EOS message")
+                    return
+                if node_id not in self.eos_flags:
+                    self.eos_flags[node_id] = True
+                    logger.info(f"EOS received for node {node_id}.")
+                else:
+                    logger.warning(f"EOS message for node {node_id} already received. Ignoring duplicate.")
+                    return
+                if len(self.eos_flags) == int(self.eos_to_await):
+                    logger.info("All nodes have sent EOS. Calculating results...")
+                    self._calculate_and_publish_results()
+                self.rabbitmq_processor.acknowledge(method)
+                return
+
+            movies_batch = json.loads(body)
+            if not isinstance(movies_batch, list):
+                logger.info("❌ Expected a list (batch) of movies, skipping.")
+                self.rabbitmq_processor.acknowledge(method)
+                return
+            for movie in movies_batch:
+                production_countries = movie.get("production_countries", [])
+                if not production_countries:
+                    continue
+
+                country = production_countries[0].get("name")
+                if not country:
+                    continue
+
+                budget = movie.get("budget", 0)
+                self.budget_by_country[country] += budget
+
+            self.rabbitmq_processor.acknowledge(method)
+
+        except json.JSONDecodeError:
+            logger.warning("❌ Skipping invalid JSON")
+            self.rabbitmq_processor.acknowledge(method)
 
 
     def process(self):
@@ -64,61 +108,24 @@ class SoloCountryBudgetQuery:
         """
         logger.info("Node is online")
 
-        def callback(ch, method, properties, body):
-            try:
-                msg_type = properties.type if properties and properties.type else "UNKNOWN"
 
-                if msg_type == EOS_TYPE:
-                    try:
-                        data = json.loads(body)
-                        node_id = data.get("node_id")
-                    except json.JSONDecodeError:
-                        logger.error("Failed to decode EOS message")
-                        return
-                    if node_id not in self.eos_flags:
-                        self.eos_flags[node_id] = True
-                        logger.info(f"EOS received for node {node_id}.")
-                    else:
-                        logger.warning(f"EOS message for node {node_id} already received. Ignoring duplicate.")
-                        return
-                    if len(self.eos_flags) == int(self.eos_to_await):
-                        logger.info("All nodes have sent EOS. Calculating results...")
-                        self._calculate_and_publish_results()
-                    ch.basic_ack(delivery_tag=method.delivery_tag)
-                    return
+        for key, value in self.config["DEFAULT"].items():
+            logger.info(f"{key}: {value}")
 
-                movies_batch = json.loads(body)
-                if not isinstance(movies_batch, list):
-                    logger.info("❌ Expected a list (batch) of movies, skipping.")
-                    ch.basic_ack(delivery_tag=method.delivery_tag)
-                    return
-                for movie in movies_batch:
-                    production_countries = movie.get("production_countries", [])
-                    if not production_countries:
-                        continue
-
-                    country = production_countries[0].get("name")
-                    if not country:
-                        continue
-
-                    budget = movie.get("budget", 0)
-                    self.budget_by_country[country] += budget
-
-                ch.basic_ack(delivery_tag=method.delivery_tag)
-
-            except json.JSONDecodeError:
-                logger.warning("❌ Skipping invalid JSON")
-                ch.basic_ack(delivery_tag=method.delivery_tag)
-
-        self.channel.basic_consume(queue=self.config["DEFAULT"]["movies_solo_queue"], on_message_callback=callback)
+        if not self.rabbitmq_processor.connect():
+            logger.error("Error al conectar a RabbitMQ. Saliendo.")
+            return
 
         try:
-            self.channel.start_consuming()
+            logger.info("Starting message consumption...")
+            self.rabbitmq_processor.consume(self.callback)
         except KeyboardInterrupt:
             logger.info("Shutting down gracefully...")
-            self.channel.stop_consuming()
+            self.rabbitmq_processor.stop_consuming()
         finally:
-            self.connection.close()
+            logger.info("Closing RabbitMQ connection...")
+            self.rabbitmq_processor.close()
+            logger.info("Connection closed.")
 
 
 
