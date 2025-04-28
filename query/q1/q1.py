38d4c5a96d1bf50a0360e85375fe0203ea90dbcd
nathencinoza <nencinoza@fi.uba.ar>
2025-04-27 22:23:15 -0300

    q2 uses mom

--- query/q1/q1.py
+++ query/q1/q1.py
@@ -3,6 +3,7 @@ import json
 import os
 import pika
 from common.logger import get_logger
+from common.mom import RabbitMQProcessor
 
 EOS_TYPE = "EOS"
 
@@ -17,17 +18,18 @@ class ArgSpainGenreQuery:
         self.config = config
         self.results = []
 
-        rabbitmq_host = self.config["DEFAULT"].get("rabbitmq_host", "rabbitmq")
-        input_queue = self.config["DEFAULT"].get("movies_arg_spain_2000s_queue", "movies_arg_spain_2000s")
-
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
-        self.channel = self.connection.channel()
+        self.source_queue = self.config["DEFAULT"].get("movies_arg_spain_2000s_queue", "movies_arg_spain_2000s")
+        self.target_queue = self.config["DEFAULT"].get("results_queue", "results_queue")
 
         self.eos_to_await = int(os.getenv("NODES_TO_AWAIT", "1"))
         self.eos_flags = {}
 
-        self.channel.queue_declare(queue=input_queue)
-        self.channel.queue_declare(queue=config["DEFAULT"]["results_queue"])
+        self.rabbitmq_processor = RabbitMQProcessor(
+            config=self.config,
+            source_queues=self.source_queue,
+            target_queues=self.target_queue
+        )   
+        
 
     def _calculate_and_publish_results(self):
         results = {
@@ -36,11 +38,11 @@ class ArgSpainGenreQuery:
         }
         logger.info("RESULTS:" + str(results))
 
-        self.channel.basic_publish(
-            exchange='',
-            routing_key=self.config["DEFAULT"]["results_queue"],
-            body=json.dumps(results),
+        self.rabbitmq_processor.publish(
+            queue=self.config["DEFAULT"]["results_queue"],
+            message=results
         )
+        
 
     def process_batch(self, movies_batch):
         """
@@ -51,6 +53,47 @@ class ArgSpainGenreQuery:
             title = movie.get("original_title")
             genres = [g.get("name") for g in movie.get("genres", []) if g.get("name")]
             self.results.append((title, genres))
+
+    def callback(self, ch, method, properties, body, input_queue):
+    
+        msg_type = properties.type if properties and properties.type else "UNKNOWN"
+
+        if msg_type == EOS_TYPE:
+            try:
+                data = json.loads(body)
+                node_id = data.get("node_id")
+            except json.JSONDecodeError:
+                logger.error("Failed to decode EOS message")
+                return
+            if node_id not in self.eos_flags:
+                self.eos_flags[node_id] = True
+                logger.info(f"EOS received for node {node_id}.")
+            else:
+                logger.warning(f"EOS message for node {node_id} already received. Ignoring duplicate.")
+                return
+            if len(self.eos_flags) == int(self.eos_to_await):
+                logger.info("All nodes have sent EOS. Calculating results...")
+                self._calculate_and_publish_results()
+            self.rabbitmq_processor.acknowledge(method)
+            return
+
+        try:
+            movies_batch = json.loads(body)
+            if not isinstance(movies_batch, list):
+                logger.warning("❌ Expected a list (batch) of movies, skipping.")
+                self.rabbitmq_processor.acknowledge(method)
+                return
+
+            # Process the batch of movies
+            self.process_batch(movies_batch)
+
+        except json.JSONDecodeError:
+            logger.warning("❌ Skipping invalid JSON")
+            self.rabbitmq_processor.acknowledge(method)
+            return
+
+        self.rabbitmq_processor.acknowledge(method)
+
 
     def process(self):
         """
@@ -64,55 +107,20 @@ class ArgSpainGenreQuery:
         """
         logger.info("Node is online")
 
-        def callback(ch, method, properties, body):
-            msg_type = properties.type if properties and properties.type else "UNKNOWN"
-
-            if msg_type == EOS_TYPE:
-                try:
-                    data = json.loads(body)
-                    node_id = data.get("node_id")
-                except json.JSONDecodeError:
-                    logger.error("Failed to decode EOS message")
-                    return
-                if node_id not in self.eos_flags:
-                    self.eos_flags[node_id] = True
-                    logger.info(f"EOS received for node {node_id}.")
-                else:
-                    logger.warning(f"EOS message for node {node_id} already received. Ignoring duplicate.")
-                    return
-                if len(self.eos_flags) == int(self.eos_to_await):
-                    logger.info("All nodes have sent EOS. Calculating results...")
-                    self._calculate_and_publish_results()
-                ch.basic_ack(delivery_tag=method.delivery_tag)
-                return
-
-            try:
-                movies_batch = json.loads(body)
-                if not isinstance(movies_batch, list):
-                    logger.warning("❌ Expected a list (batch) of movies, skipping.")
-                    ch.basic_ack(delivery_tag=method.delivery_tag)
-                    return
-
-                # Process the batch of movies
-                self.process_batch(movies_batch)
-
-            except json.JSONDecodeError:
-                logger.warning("❌ Skipping invalid JSON")
-                ch.basic_ack(delivery_tag=method.delivery_tag)
-                return
-
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-
-        self.channel.basic_consume(queue=self.config["DEFAULT"]["movies_arg_spain_2000s_queue"], on_message_callback=callback)
+        if not self.rabbitmq_processor.connect():
+            logger.error("Error al conectar a RabbitMQ. Saliendo.")
+            return
 
         try:
-            self.channel.start_consuming()
+            logger.info("Starting message consumption...")
+            self.rabbitmq_processor.consume(self.callback)
         except KeyboardInterrupt:
             logger.info("Shutting down gracefully...")
-            self.channel.stop_consuming()
+            self.rabbitmq_processor.stop_consuming()
         finally:
-            self.connection.close()
-
+            logger.info("Closing RabbitMQ connection...")
+            self.rabbitmq_processor.close()
+            logger.info("Connection closed.")
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
