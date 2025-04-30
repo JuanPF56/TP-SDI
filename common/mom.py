442b57b149551d2d512f9c2ab0f829be112cdfce
Juan Pablo Fresia <39677236+JuanPF56@users.noreply.github.com>
2025-04-29 23:55:45 -0300

    Refactor: update RabbitMQProcessor and YearFilter to support exchanges

--- common/mom.py
+++ common/mom.py
@@ -11,10 +11,25 @@ HEARTBEAT = 600
 CONNECTION_TIMEOUT = 300
 
 class RabbitMQProcessor:
-    def __init__(self, config, source_queues, target_queues, rabbitmq_host="rabbitmq"):
+    def __init__(self, config, source_queues, target_queues, rabbitmq_host="rabbitmq",
+                 source_exchange=None, exchange_queues=None, target_exchange=None):
+        """
+        Initializes the RabbitMQProcessor with the given configuration and queues.
+        :param config: Configuration object containing RabbitMQ settings.
+        :param source_queues: List of source queues to consume messages from.
+        :param target_queues: List of target queues to publish messages to.
+        :param rabbitmq_host: Hostname of the RabbitMQ server.
+        :param source_exchange: Name of the source exchange to bind queues to.
+        :param exchange_queues: List of queues to bind to the source exchange (they have
+        to be in the source_queues list).
+        :param target_exchange: Name of the target exchange to publish messages to.
+        """
         self.config = config
         self.source_queues = source_queues if isinstance(source_queues, list) else [source_queues]
         self.target_queues = target_queues 
+        self.source_exchange = source_exchange
+        self.target_exchange = target_exchange
+        self.exchange_queues = exchange_queues if isinstance(exchange_queues, list) else [exchange_queues]
         self.rabbitmq_host = rabbitmq_host
         self.connection = None
         self.channel = None
@@ -55,6 +70,18 @@ class RabbitMQProcessor:
                         else:
                             self.channel.queue_declare(queue=target)
 
+                # Handle source_exchange and target_exchange if provided
+                if self.source_exchange:
+                    self.channel.exchange_declare(exchange=self.source_exchange, exchange_type='topic')
+                    # If exchange_queues is not a list, convert it to a list
+                    if not isinstance(self.exchange_queues, list):
+                        self.exchange_queues = [self.exchange_queues]
+                    for queue in self.exchange_queues:
+                        self.channel.queue_bind(exchange=self.source_exchange, queue=queue, routing_key=queue)
+                
+                if self.target_exchange:
+                    self.channel.exchange_declare(exchange=self.target_exchange, exchange_type='topic')                       
+                
                 return True
 
             except pika.exceptions.AMQPConnectionError as e:
@@ -71,7 +98,7 @@ class RabbitMQProcessor:
 
     def consume(self, callback):
         """
-        Inicia el consumo de mensajes desde las colas.
+        Starts consuming messages from the specified source queues.
         """
         logger.info("Iniciando el consumo de mensajes...")
         for queue in self.source_queues:
@@ -91,18 +118,18 @@ class RabbitMQProcessor:
         logger.info("Esperando mensajes...")
         self.channel.start_consuming()
         
-    def publish(self, queue, message, msg_type=None):
+    def publish(self, target, message, msg_type=None, exchange=False):
         """
-        Publica un mensaje en la cola especificada.
-        """
-        logger.debug(f"Publicando mensaje en la cola: {queue}, tipo: {msg_type}", extra={"payload": message})
+        Publishes a message to the specified target.
+        """        
+        logger.debug(f"Publicando mensaje en: {target}, tipo: {msg_type}", extra={"payload": message})
         self.channel.basic_publish(
-            exchange='',
-            routing_key=queue,
+            exchange=target if exchange else '',
+            routing_key=target if not exchange else '',
             body=json.dumps(message),
             properties=pika.BasicProperties(type=msg_type)
         )
-        logger.debug(f"Mensaje enviado a la cola: {queue}, tipo: {msg_type}")
+        logger.debug(f"Mensaje enviado a: {target}, tipo: {msg_type}")
 
     def stop_consuming(self):
         """
