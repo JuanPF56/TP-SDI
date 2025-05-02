d72322127aa1f7d26368f9c13707489b5d5a1809
nathencinoza <nencinoza@fi.uba.ar>
2025-05-02 16:03:24 -0300

    changes in client state

--- common/client_state.py
+++ common/client_state.py
@@ -1,4 +1,7 @@
 # common/client_state.py
+from common.logger import get_logger
+
+logger = get_logger("Client-State")
 
 class ClientState:
     def __init__(self, client_id):
@@ -9,6 +12,9 @@ class ClientState:
     def add_batch(self, data):
         self.batches.extend(data)
 
+    def clear_batch(self):
+        self.batches.clear()
+
     def mark_eos(self, queue_name):
         self.eos_flags[queue_name] = True
 
@@ -16,4 +22,5 @@ class ClientState:
         return self.eos_flags.get(queue_name, False)
 
     def all_queues_eos(self, expected_queues):
+
         return all(self.eos_flags.get(q, False) for q in expected_queues)
