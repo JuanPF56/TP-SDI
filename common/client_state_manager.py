80752f5d8dfd22edd0ab4e39b3588123e3bfebfb
nathencinoza <nencinoza@fi.uba.ar>
2025-05-02 16:03:01 -0300

    changes in client state manager

--- common/client_state_manager.py
+++ common/client_state_manager.py
@@ -6,15 +6,14 @@ class ClientManager:
         self.clients = {}  # key: client_id, value: ClientState
         self.expected_queues = expected_queues
 
-    def get_or_create(self, client_id):
+    def add_client(self, client_id):
         if client_id not in self.clients:
             self.clients[client_id] = ClientState(client_id)
         return self.clients[client_id]
 
-    def mark_eos(self, client_id, queue_name):
-        client = self.get_or_create(client_id)
-        client.mark_eos(queue_name)
-
-    def client_received_all_eos(self, client_id):
-        client = self.get_or_create(client_id)
-        return client.all_queues_eos(self.expected_queues)
+    def get_client(self, client_id) -> ClientState:
+        return self.clients.get(client_id)
+    
+    def remove_client(self, client_id):
+        if client_id in self.clients:
+            del self.clients[client_id]
