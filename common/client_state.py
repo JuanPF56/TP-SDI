49a5f46c04fda54c744e9400b1694bb982ad39eb
nathencinoza <nencinoza@fi.uba.ar>
2025-05-03 11:39:57 -0300

    update client state

--- common/client_state.py
+++ common/client_state.py
@@ -4,23 +4,53 @@ from common.logger import get_logger
 logger = get_logger("Client-State")
 
 class ClientState:
-    def __init__(self, client_id):
+    def __init__(self, client_id, nodes_to_await=1):
         self.client_id = client_id
-        self.batches = [] 
-        self.eos_flags = {}  # key: queue_name, value: bool
+        self.eos_flags = {}  # key: queue_name, value: dict(node_id: bool)  
+        self.results = {} # this is optional, only the query needs it
+        self.amount_of_eos = nodes_to_await
 
-    def add_batch(self, data):
-        self.batches.extend(data)
+    def mark_eos(self, queue_name, node_id = 1):
+        if queue_name not in self.eos_flags:
+            self.eos_flags[queue_name] = {}
+        self.eos_flags[queue_name][node_id] = True
 
-    def clear_batch(self):
-        self.batches.clear()
+    def has_queue_received_eos(self, queue_name): ## chequear if amount of eos is correct
+        logger.info(f"Checking if all EOS received for queue {queue_name}")
+        eos_received = self.eos_flags.get(queue_name, {})
+        logger.info(f"EOS received: {eos_received}")
+        if len(eos_received) == self.amount_of_eos:
+            logger.info(f"All EOS received for queue {queue_name}")
+            return True
+    def has_queue_received_eos_from_node(self, queue_name, node_id):
+        logger.info(f"Checking if EOS received from node {node_id} for queue {queue_name}")
+        eos_received = self.eos_flags.get(queue_name, {})
+        logger.info(f"EOS received: {eos_received}")
+        if eos_received.get(node_id, False):
+            logger.info(f"EOS received from node {node_id} for queue {queue_name}")
+            return True
+        else:
+            logger.info(f"EOS not received from node {node_id} for queue {queue_name}")
 
-    def mark_eos(self, queue_name):
-        self.eos_flags[queue_name] = True
+        
+    def has_received_all_eos(self, queues):
+        logger.info(f"Checking if all EOS received for queues {queues}")
+        if not isinstance(queues, list):
+            queues = [queues]
+        for queue in queues:
+            if not self.has_queue_received_eos(queue):
+                logger.info(f"Not all EOS received for queue {queue}")
+                return False
+        logger.info("All EOS received for all queues")
+        return True    
 
-    def has_received_eos(self, queue_name):
-        return self.eos_flags.get(queue_name, False)
 
-    def all_queues_eos(self, expected_queues):
+    def add_result(self, results):
+        for key, value in results.items():
+            if key not in self.results:
+                self.results[key] = 0
+            self.results[key] += value
 
-        return all(self.eos_flags.get(q, False) for q in expected_queues)
+    def get_results(self):
+        return self.results
+
