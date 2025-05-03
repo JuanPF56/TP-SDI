f3e96096a4f5160dc8a09d24cd56c9c5f2648971
nathencinoza <nencinoza@fi.uba.ar>
2025-05-03 11:39:41 -0300

    update state managger

--- common/client_state_manager.py
+++ common/client_state_manager.py
@@ -2,18 +2,18 @@
 from common.client_state import ClientState
 
 class ClientManager:
-    def __init__(self, expected_queues):
-        self.clients = {}  # key: client_id, value: ClientState
-        self.expected_queues = expected_queues
-
-    def add_client(self, client_id):
-        if client_id not in self.clients:
-            self.clients[client_id] = ClientState(client_id)
-        return self.clients[client_id]
-
-    def get_client(self, client_id) -> ClientState:
-        return self.clients.get(client_id)
+    def __init__(self, expected_queues, nodes_to_await=1):
     
-    def remove_client(self, client_id):
-        if client_id in self.clients:
-            del self.clients[client_id]
+        self.clients = {}  # key: (client_id, request_id), value: ClientState
+        self.expected_queues = expected_queues
+        self.nodes_to_await = nodes_to_await
+
+    def add_client(self, client_id, request_id) -> ClientState:
+        key = (client_id, request_id)
+        if key not in self.clients:
+            self.clients[key] = ClientState(client_id, self.nodes_to_await)
+        return self.clients[key]
+    
+    def remove_client(self, client):
+        if client in self.clients:
+            del self.clients[client]
