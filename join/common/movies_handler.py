fdd3f623afc47ae0333853119c748254f46c99c6
Juan Pablo Fresia <39677236+JuanPF56@users.noreply.github.com>
2025-05-03 19:53:36 -0300

    Refactor EOS handling in JoinBase and MoviesHandler to support multiple clients and improve message processing

--- join/common/movies_handler.py
+++ join/common/movies_handler.py
@@ -2,8 +2,10 @@ import json
 import multiprocessing
 import signal
 
-from common import logger
+from common.logger import get_logger
 from common.mom import RabbitMQProcessor
+
+logger = get_logger("MoviesHandler")
 
 EOS_TYPE = "EOS"
 
@@ -33,6 +35,9 @@ class MoviesHandler(multiprocessing.Process):
         self.ready = False
         self.movies_table_ready = ready_event
 
+        self.current_client_id = None
+        self.current_request_number = None
+
     def __handleSigterm(self, signum, frame):
         self.rabbitmq_processor.stop_consuming()
         self.rabbitmq_processor.close()
@@ -48,75 +53,84 @@ class MoviesHandler(multiprocessing.Process):
         def callback(ch, method, properties, body, queue_name):
             try:
                 msg_type = properties.type if properties and properties.type else "UNKNOWN"
+                headers = getattr(properties, "headers", {}) or {}
+                self.current_client_id = headers.get("client_id")
+                self.current_request_number = headers.get("request_number")
+                id_tuple = (self.current_client_id, self.current_request_number)
+
+                if not self.current_client_id or not self.current_request_number:
+                    logger.error("Missing client_id or request_number in headers")
+                    self.rabbitmq_processor.acknowledge(method)
+                    return
                 if msg_type == EOS_TYPE:
                     try:
                         data = json.loads(body)
                         node_id = data.get("node_id")
-                        client_id = 1 # TODO: Handle for each client
                     except json.JSONDecodeError:
-                        self.log_error("Failed to decode EOS message")
+                        logger.error("Failed to decode EOS message")
                         return
-                    if client_id not in self.year_eos_flags:
-                        self.year_eos_flags[client_id] = {}
-                    if node_id not in self.year_eos_flags[client_id]:
-                        self.year_eos_flags[client_id][node_id] = True
-                        self.log_debug(f"EOS received for node {node_id}.")
-                    if not self.ready and self.client_ready(client_id):
+                    if id_tuple not in self.year_eos_flags:
+                        self.year_eos_flags[id_tuple] = self.manager.dict()
+                    if node_id not in self.year_eos_flags[id_tuple]:
+                        self.year_eos_flags[id_tuple][node_id] = True
+                        logger.debug(f"EOS received for node {node_id}.")
+                    if not self.ready and self.client_ready(self.current_client_id, self.current_request_number):
                         self.ready = True
-                        self.log_info("One table is ready for at least 1 client. Notifying...")
+                        logger.debug("One table is ready for at least 1 client. Notifying...")
                         self.movies_table_ready.set()            
                 else:
                     try:
                         data = json.loads(body)
-                        # TODO: Handle for each client
-                        client_id = 1
                         movies_data = data
                     except json.JSONDecodeError:
-                        self.log_error(f"Error decoding JSON: {body}")
+                        logger.error(f"Error decoding JSON: {body}")
                         return
                     logger.debug(f"Received message: {movies_data}")
-                    if client_id not in self.movies:
-                        self.movies[client_id] = self.manager.list()
+                    if id_tuple not in self.movies:
+                        self.movies[id_tuple] = self.manager.list()
                     for movie in movies_data:
                         new_movie = {
                             "id" : str(movie["id"]),
                             "original_title": movie["original_title"],
                         }
-                        self.movies[client_id].append(new_movie)
-                    logger.debug(f"Movies table updated for client {client_id}: {self.movies[client_id]}")
+                        self.movies[id_tuple].append(new_movie)
+                    logger.debug(f"Movies table updated for client {self.current_client_id}, \
+                        request {self.current_request_number}: {self.movies[id_tuple]}")
             except Exception as e:
-                self.log_error(f"Error processing message: {e}")
+                logger.error(f"Error processing message: {e}")
             finally:
                 self.rabbitmq_processor.acknowledge(method)
                 
         self.rabbitmq_processor.consume(callback)
-
         self.rabbitmq_processor.close()
 
-    def client_ready(self, client_id):
+    def client_ready(self, client_id, request_number):
         """
         Check if all nodes have sent EOS messages for the given client ID.
         """
-        if client_id not in self.year_eos_flags:
+        id_tuple = (client_id, request_number)
+        if id_tuple not in self.year_eos_flags:
             return False
-        return len(self.year_eos_flags[client_id]) == int(self.year_nodes_to_await)
+        return len(self.year_eos_flags[id_tuple]) == int(self.year_nodes_to_await)
     
-    def get_movies_table(self, client_id):
+    def get_movies_table(self, client_id, request_number):
         """
         Get the movies table for the given client ID.
         """
-        if client_id not in self.movies:
+        id_tuple = (client_id, request_number)
+        if id_tuple not in self.movies:
             return None
-        return list(self.movies[client_id])
+        return list(self.movies[id_tuple])
     
-    def remove_movies_table(self, client_id):
+    def remove_movies_table(self, client_id, request_number):
         """
         Remove the movies table for the given client ID.
         """
-        if client_id in self.movies:
-            del self.movies[client_id]
-            logger.debug(f"Movies table removed for client {client_id}.")
+        id_tuple = (client_id, request_number)
+        if id_tuple in self.movies:
+            del self.movies[id_tuple]
+            logger.debug(f"Movies table removed for client {client_id}, request {request_number}.")
         else:
-            logger.error(f"No movies table found for client {client_id}.")
+            logger.error(f"No movies table found for client {client_id}, request {request_number}.")
 
     
