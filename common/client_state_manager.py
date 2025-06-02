a3b1298addce8f32ad9f56b1f066927b952b2091
csebellin@fi.uba.ar <csebellin@fi.uba.ar>
2025-06-02 16:30:15 -0300

    Refactor ClientManager: Simplify client management by removing request_id from add_client and remove_client methods

--- common/client_state_manager.py
+++ common/client_state_manager.py
@@ -1,23 +1,32 @@
 # common/client_manager.py
 from common.client_state import ClientState
 
+
 class ClientManager:
     def __init__(self, expected_queues, nodes_to_await=1):
-    
-        self.clients = {}  # key: (client_id, request_id), value: ClientState
+
+        self.clients = {}  # key: (client_id), value: ClientState
         self.expected_queues = expected_queues
         self.nodes_to_await = nodes_to_await
 
-    def add_client(self, client_id, request_id, is_eos=False) -> ClientState:
-        key = (client_id, request_id)
+    def add_client(self, client_id, is_eos=False) -> ClientState:
+        """
+        Add a new client or return the existing client state.
+        If is_eos is True, it will not create a new client state.
+        """
+        key = client_id
         if key not in self.clients:
             if is_eos:
                 return None
             else:
-                self.clients[key] = ClientState(client_id, request_id, self.nodes_to_await)
+                self.clients[key] = ClientState(client_id, self.nodes_to_await)
         return self.clients[key]
-    
-    def remove_client(self, client_id, request_id):
-        key = (client_id, request_id)
+
+    def remove_client(self, client_id):
+        """
+        Remove a client from the manager.
+        This will delete the client state from the manager.
+        """
+        key = client_id
         if key in self.clients:
             del self.clients[key]
